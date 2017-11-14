a96a5bf282cd955436a3e962886a6c6ee718a932
Francesco Gianni <francesco.gianni@objectway.it>
2017-11-14 10:51:00 +0100

    suppress warnings

--- src/main/java/com/teamtreehouse/giflib/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/CategoryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@SuppressWarnings("unused")
 public class CategoryController {
 
     @Autowired
--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@SuppressWarnings("unused")
 public class GifController {
 
     @Autowired
